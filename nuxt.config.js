d07ac8248730c0cfe55a9ec6de7f1d17b635bd35
Dogukan Cirpan <cirpan.dogukan5959@gmail.com>
2022-09-27 00:18:49 +0200

    (feat/components) :sparkles: create header & about component and import in index

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,9 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
+      options: {
+        customProperties: true
+      },
       dark: true,
       themes: {
         dark: {
